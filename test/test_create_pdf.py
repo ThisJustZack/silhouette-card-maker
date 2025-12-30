cb94677ff9ea25c039104c5ff9a9c98662c17979
ThisJustZack <34222941+ThisJustZack@users.noreply.github.com>
2025-12-30 10:07:45 -0500

    Remap unit tests
    Remap unit tests so that they cna function as they did before

--- test/test_create_pdf.py
+++ test/test_create_pdf.py
@@ -1,9 +1,9 @@
 import os
 from click.testing import CliRunner
-from create_pdf import cli
+from commands.create_pdf import create_pdf_cli
 
 def test_basic_create_pdf():
   runner = CliRunner()
-  result = runner.invoke(cli, "--front_dir_path test/basic/front --back_dir_path test/basic/back --output_path test/basic/output/game.pdf")
+  result = runner.invoke(create_pdf_cli, "--front_dir_path test/basic/front --back_dir_path test/basic/back --output_path test/basic/output/game.pdf")
   assert result.exit_code == 0
   assert os.path.exists("test/basic/output/game.pdf")
