8703706a27a0baaa870708b09fea61111808f7c0
ThisJustZack <34222941+ThisJustZack@users.noreply.github.com>
2026-08-05 18:05:05 -0400

    Add double sided support to Flesh and Blood

--- plugins/flesh_and_blood/infrastructure/FleshAndBloodImageSearcher.py
+++ plugins/flesh_and_blood/infrastructure/FleshAndBloodImageSearcher.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from typing import Optional, Mapping, Any
 from re import sub
-from unicodedata import normalize
 
 from plugins.flesh_and_blood.domain.FleshAndBloodCard import FleshAndBloodCard
 from plugins.flesh_and_blood.domain.Pitch import Pitch
+from plugins.abstraction_base.domain.CardFace import CardFace
 from plugins.abstraction_base.domain.CardImage import CardImage
 from plugins.abstraction_base.infrastructure.ImageCachePort import DEFAULT_IMAGE_CACHE_PATH, DEFAULT_IMAGE_CONTENT_TYPE
 from plugins.abstraction_base.infrastructure.ImageSearcherPort import ImageSearcherLike
@@ -15,9 +15,9 @@ from plugins.abstraction_base.infrastructure.WebRequest import perform_web_reque
 FLESH_AND_BLOOD_API_URL_TEMPLATE = 'https://api.cardvault.fabtcg.com/carddb/api/v1/advanced-search/?name={CARD_NAME}{PITCH}'
 
 class FleshAndBloodImageSearcher(ImageSearcherLike[FleshAndBloodCard]):
-    async def find_image(self, card: FleshAndBloodCard, face) -> Optional[CardImage]:
+    async def find_image(self, card: FleshAndBloodCard, face: CardFace) -> Optional[CardImage]:
 
-        print(card.name)
+        print(card.id, face)
         sanitized = sub(r'[^A-Za-z0-9 ]+', '', card.name)
         slugified = sub(r'\s+', '+', sanitized).lower()
         pitch_argument = f'&pitch_lookup=exact&pitch={card.pitch.value}' if card.pitch != Pitch.NONE else ''
@@ -28,11 +28,18 @@ class FleshAndBloodImageSearcher(ImageSearcherLike[FleshAndBloodCard]):
             json_response = request_response.json()
             card_from_response: Mapping[str, Any] = json_response.get('results')[0] if len(json_response.get('results')) > 0 else {}
 
-            image_url = card_from_response.get('faces', [{}])[0].get('image', {}).get('large', None)
+            card_faces = card_from_response.get('faces', [{}, {}])
+            match face:
+                case CardFace.FRONT:
+                    if len(card_faces) < 1: return None
+                case CardFace.BACK: 
+                    if len(card_faces) < 2: return None
+
+            image_url = card_faces[1 if face == CardFace.BACK else 0].get('image', {}).get('large', None)
 
             if image_url != None:
                 card_image = await perform_web_request(image_url)
 
                 return CardImage(filename = DEFAULT_IMAGE_CACHE_PATH.format(CARD_ID=card.id),
                                 content_type = DEFAULT_IMAGE_CONTENT_TYPE,
-                                data = card_image.content)
+                                data = card_image.content) if card_image != None else None
